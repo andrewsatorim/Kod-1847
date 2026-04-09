aaf8a56f8a4a5085a1b94d108f044b1107ac625b
andrewsatorim <andrew.mordovkin@gmail.com>
2026-04-09 23:35:13 +0300

    feat: only Hero screen on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
 "use client";
 import { useState } from "react";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Philosophy from "@/components/Philosophy";
-import Halls from "@/components/Halls";
-import MenuPreview from "@/components/MenuPreview";
-import EventsPreview from "@/components/EventsPreview";
-import Gallery from "@/components/Gallery";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
 import ReservationModal from "@/components/ReservationModal";
 
 export default function Home() {
@@ -16,15 +8,7 @@ export default function Home() {
 
   return (
     <>
-      <Header />
       <Hero onReserve={() => setModalOpen(true)} />
-      <Philosophy />
-      <Halls />
-      <MenuPreview />
-      <EventsPreview />
-      <Gallery />
-      <Contact />
-      <Footer />
       <ReservationModal open={modalOpen} onClose={() => setModalOpen(false)} />
     </>
   );
