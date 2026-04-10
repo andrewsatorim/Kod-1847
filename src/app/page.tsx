e1ec65c312c38d407e31b7a350ed492ea0b56099
andrewsatorim <andrew.mordovkin@gmail.com>
2026-04-10 18:41:03 +0300

    feat: add EventsPreview section to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Philosophy from "@/components/Philosophy";
 import RoomPreviews from "@/components/RoomPreviews";
+import EventsPreview from "@/components/EventsPreview";
 import ReservationModal from "@/components/ReservationModal";
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
       <Hero onReserve={() => setModalOpen(true)} />
       <Philosophy />
       <RoomPreviews />
+      <EventsPreview />
       <ReservationModal open={modalOpen} onClose={() => setModalOpen(false)} />
     </>
   );
