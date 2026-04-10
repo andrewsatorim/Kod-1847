396013a929f9a2a1c230055e3e1e400c09c5eec3
andrewsatorim <andrew.mordovkin@gmail.com>
2026-04-10 13:05:52 +0300

    fix: remove Halls section from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Philosophy from "@/components/Philosophy";
-import Halls from "@/components/Halls";
 import ReservationModal from "@/components/ReservationModal";
 
 export default function Home() {
@@ -14,7 +13,6 @@ export default function Home() {
       <Header />
       <Hero onReserve={() => setModalOpen(true)} />
       <Philosophy />
-      <Halls />
       <ReservationModal open={modalOpen} onClose={() => setModalOpen(false)} />
     </>
   );
